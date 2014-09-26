62b7f24c43e349fb7b541cd0cc7571a3233a9706
root <root@mimir.med.upenn.edu>
2014-09-26 10:39:16 -0400

    Exclude performance_schema (5.6+)

--- hot-indy-backup.sh
+++ hot-indy-backup.sh
@@ -26,10 +26,7 @@ UTILDIR='/home/mysql/util'
 DUMPOPTS="--net_buffer_length=16M --max_allowed_packet=200M --quick --opt  --extended-insert=FALSE "
 
 # list of Databases to exclude, by default all DBs are backed up
-EXCLUDES="information_schema test"
-
-# note gme_evals and ume_evals are huge and need to be backed up separately.
-
+EXCLUDES="information_schema performance_schema test"
 
 
 # Set a tag to label this round of backups
